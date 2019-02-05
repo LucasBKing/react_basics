e30c7c6881da3234e8e7ef905c87b4f684fe3eb8
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-05 20:47:28 -0200

    materialize added, show todos and delete

--- src/App.js
+++ src/App.js
@@ -1,46 +1,31 @@
 import React, { Component } from 'react';
-import Ninjas from './Ninja'
-import AddNinja from './AddNinja'
+import Todos from './Todos'
 
 class App extends Component {
   state = {
-    ninjas: [
-      {id: 1, name: 'Name one', age: 10, belt: 'white'},
-      {id: 2, name: 'Name two', age: 12, belt: 'brown'},
-      {id: 3, name: 'Name three', age: 14, belt: 'black'}
+    todos: [
+      {id: 1, content: 'Go to store'},
+      {id: 2, content: 'Finish React Tutorials'}
     ]
   }
 
-  addNinja = (newNinja) => {
-    newNinja.id = Math.random();
-    let ninjas = [...this.state.ninjas, newNinja]
+  deleteTodo = (id) => {
+    let todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+
     this.setState({
-      ninjas: ninjas
+      todos
     });
   }
 
-  deleteNinja = (id) => {
-    let ninjas = this.state.ninjas.filter( ninja => {
-      return ninja.id !== id
-    })
-    this.setState({
-      ninjas
-    })
-  }
-
-  componentDidMount() {
-    console.log(`Component mounted`);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('Component update');
-    console.log(prevProps, prevState);
-  }
   render() {
     return (
-      <div className="App">
-        <Ninjas ninjas={ this.state.ninjas } deleteNinja={ this.deleteNinja }/>
-        <AddNinja addNinja={ this.addNinja }/>
+      <div className="todo-app container">
+        <h1 className="center blue-text">
+          Todo's
+        </h1>
+        <Todos todos={this.state.todos} deleteTodo={ this.deleteTodo } />
       </div>
     );
   }
