d86bd90f1c0af637468a404c581f08be10a820b0
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-05 18:18:18 -0200

    third contidional output way

--- src/Ninja.js
+++ src/Ninja.js
@@ -19,20 +19,30 @@ const Ninjas = ({ninjas}) => {
         
     // });
 
-    let ninjaList = ninjas.map(ninja => {
-        // condition ? (if true) : (if false)
-        return ninja.age > 13 ? (
-            <div className="ninja" key={ninja.id}>
-                <div>Name: { ninja.name }</div>
-                <div>Age: { ninja.age }</div>
-                <div>Belt: { ninja.belt }</div>
-            </div>
-        ) : null;
-    });
+    // let ninjaList = ninjas.map(ninja => {
+    //     // condition ? (if true) : (if false)
+        // return ninja.age > 13 ? (
+        //     <div className="ninja" key={ninja.id}>
+        //         <div>Name: { ninja.name }</div>
+        //         <div>Age: { ninja.age }</div>
+        //         <div>Belt: { ninja.belt }</div>
+        //     </div>
+        // ) : null;
+    // });
 
     return(
         <div className="ninja-list">
-            { ninjaList }
+            {
+                ninjas.map(ninja => {
+                    return ninja.age < 13 ? (
+                        <div className="ninja" key={ninja.id}>
+                            <div>Name: { ninja.name }</div>
+                            <div>Age: { ninja.age }</div>
+                            <div>Belt: { ninja.belt }</div>
+                        </div>
+                    ) : null;
+                })
+            }
         </div>
     )
 }
