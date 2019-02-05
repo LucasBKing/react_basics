b25d58d9043d5969f0dce1b3699494245dbe9f6a
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-05 14:37:12 -0200

    Handling basic DOM events

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ class App extends Component {
     age: 30
   }
 
+  handleClick(e) {
+   // console.log(e.target)
+    console.log(this.state)
+  }
+
+  handleMouseOver(e) {
+    console.log(e.target, e.pageX)
+  }
+
+  handleCopy(e) {
+    console.log('Try being original for once!')
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello World</h1>
         <p>My name is: { this.state.name } and I am { this.state.age }</p>
+        <button onClick={ this.handleClick }>Click Me</button>
+        <button onMouseOver={  this.handleMouseOver }>Hover me</button>
+        <p onCopy={ this.handleCopy }>What we think, we become</p>
       </div>
     );
   }
