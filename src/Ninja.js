85b618d82812c38376e4872738c8c9abd9074100
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-05 18:15:22 -0200

    second conditional output way

--- src/Ninja.js
+++ src/Ninja.js
@@ -3,19 +3,31 @@ import React from 'react';
 const Ninjas = ({ninjas}) => {
     //this.props.ninjas
     //let { ninjas } = props;
-    let ninjaList = ninjas.map(ninja => {
-        if( ninja.age < 13) {
-            return (
-                <div className="ninja" key={ninja.id}>
-                    <div>Name: { ninja.name }</div>
-                    <div>Age: { ninja.age }</div>
-                    <div>Belt: { ninja.belt }</div>
-                </div>
-            );
-        } else {
-            return null;
-        }
+    // first kind of conditional output
+    // let ninjaList = ninjas.map(ninja => {
+    //     if( ninja.age < 13) {
+    //         return (
+    //             <div className="ninja" key={ninja.id}>
+    //                 <div>Name: { ninja.name }</div>
+    //                 <div>Age: { ninja.age }</div>
+    //                 <div>Belt: { ninja.belt }</div>
+    //             </div>
+    //         );
+    //     } else {
+    //         return null;
+    //     }
         
+    // });
+
+    let ninjaList = ninjas.map(ninja => {
+        // condition ? (if true) : (if false)
+        return ninja.age > 13 ? (
+            <div className="ninja" key={ninja.id}>
+                <div>Name: { ninja.name }</div>
+                <div>Age: { ninja.age }</div>
+                <div>Belt: { ninja.belt }</div>
+            </div>
+        ) : null;
     });
 
     return(
