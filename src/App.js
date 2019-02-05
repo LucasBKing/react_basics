d2d4c65a2cb910cf8fce9119a75d9af87204cec4
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-05 15:31:12 -0200

    using props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Ninjas />
+        <Ninjas name="Ryu" age="25" belt="Black"/>
       </div>
     );
   }
