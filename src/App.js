af920311c4ef2da670ab2b1b8f5b2b99af697d1d
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-05 15:26:42 -0200

    creating and importing external component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninja'
 
 class App extends Component {
-  state = {
-    name: 'Ryu',
-    age: 30
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      name: e.target.value
-    });
-  }
-
-  handleSubmit = (e) => {
-    //prevent default behaviour
-    e.preventDefault()
-    console.log(`Form submitted ${this.state.name}`)
-  }
+  
 
   render() {
     return (
       <div className="App">
-        <p>My name is: { this.state.name } and I am { this.state.age }</p>
-        <form onSubmit={ this.handleSubmit }>
-          <input type="text" onChange={ this.handleChange } />
-          <button>Submit</button>
-        </form>
+        <Ninjas />
       </div>
     );
   }
