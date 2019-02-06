aafc9aec6a75234c8d6da1e3c4f550c8b56eadbc
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-06 11:29:01 -0200

    getting posts with axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+//Functional components cannot use lyfecicle hooks
+//Change it to class based component
+class Home extends Component {
+    state = {
+        posts: [ ]
+    }
 
-const Home = () => {
-    return(
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nostrum laudantium numquam quos? Expedita atque ratione quam deserunt excepturi iure dolorem dolorum odio quia dignissimos, cupiditate a possimus doloribus illo?</p>
-        </div>
-    )
+    componentDidMount() {
+        //getting just the first ten posts
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                this.setState({
+                    posts: res.data.slice(0,10)
+                });
+            });
+    }
+
+    render() {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return(
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts yet.</div>
+        );
+        return(
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+        )
+    }  
 }
 
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { Link, NavLink, withRouter } from 'react-router-dom';
 
 const Navbar = (props) => {
-    setTimeout(() => {
-        props.history.push('/about');
-    }, 2000);
+  
     // console.log(props); <- empty without withRouter
     return(
         <nav className="nav-wrapper blue darken-3">
